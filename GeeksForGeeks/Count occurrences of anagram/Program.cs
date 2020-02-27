754fd1de2e376e9a37e024e8b969d9b87f67790b
chinmay6345 <chinmay8116@gmail.com>
2020-02-27 07:14:43 +0530

    added description

--- GeeksForGeeks/Count occurrences of anagram/Program.cs
+++ GeeksForGeeks/Count occurrences of anagram/Program.cs
@@ -2,7 +2,32 @@
 using System.Linq;
 
 //https://practice.geeksforgeeks.org/problems/count-occurences-of-anagrams/0
+/*
+Given a word S and a text C. Return the count of the occurences of anagrams of the word in the text.
 
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. The first line of each test case contains a text S consisting of only lowercase characters.
+The second line contains a word consisting of only lowercase characters.
+
+Output:
+Print the count of the occurences of anagrams of the word C in the text S.
+
+Constraints:
+1 <= T <= 50
+1 <= |S| <= |C| <= 50
+
+Example:
+Input:
+2
+forxxorfxdofr
+for
+aabaabaa
+aaba
+
+Output:
+3
+4 
+*/
 namespace Count_occurrences_of_anagram
 {
     public static class AppHelper
