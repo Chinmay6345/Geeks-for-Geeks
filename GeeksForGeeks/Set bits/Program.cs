5e3d99b72bcf519fc32f1bb671885d798dc4fc8d
chinmay6345 <chinmay8116@gmail.com>
2020-03-01 07:46:24 +0530

    commit

---
+++ GeeksForGeeks/Set bits/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://practice.geeksforgeeks.org/problems/set-bits/0
+/*
+Given a positive integer N, print count of set bits in it. For example, if the given number is 6(binary: 110), output should be 2 as there are two set bits in it.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each testcase contains single line of input containing the N.
+
+Output:
+For each test case, in a new line, print count of set bits in it.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 106
+
+Example:
+Input:
+2
+6
+11
+Output:
+2
+3 
+     
+*/
+namespace Set_bits
+{
+    public static class AppHelper
+    {
+        public static void SetBits(Int32 n)
+        {
+            String binary = Convert.ToString(n, 2);
+            Console.WriteLine(binary.Where(a => a == '1').Count());
+        }
+    }
+
+    public class GFG
+    {
+        public static void Main(String[] args)
+        {
+            Int32 NoOfTestCases = Convert.ToInt32(Console.ReadLine());
+            while (NoOfTestCases != 0)
+            {
+                Int32 num = Convert.ToInt32(Console.ReadLine());
+                AppHelper.SetBits(num);
+                NoOfTestCases = NoOfTestCases - 1;
+            }
+        }
+    }
+}
--- GeeksForGeeks/Sort an array of 0s, 1s and 2s/Program.cs
+++ GeeksForGeeks/Sort an array of 0s, 1s and 2s/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-//https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s/0
+//https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s/0 
 /*
 Given an array A of size N containing 0s, 1s, and 2s; you need to sort the array in ascending order.
 
