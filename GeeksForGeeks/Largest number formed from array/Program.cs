cd860c6b03fab6a420c9d7955f4bfde464db864f
chinmay6345 <chinmay8116@gmail.com>
2020-04-07 10:53:04 +0530

    commit-added description

--- GeeksForGeeks/Largest number formed from array/Program.cs
+++ GeeksForGeeks/Largest number formed from array/Program.cs
@@ -3,7 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 
 //https://practice.geeksforgeeks.org/problems/largest-number-formed-from-an-array/0
+/*
+Given a list of non negative integers, arrange them in such a manner that they form the largest number possible.The result is going to be very large, hence return the result in the form of a string.
 
+Input:
+The first line of input consists number of the test cases. The description of T test cases is as follows:
+The first line of each test case contains the size of the array, and the second line has the elements of the array.
+
+Output:
+In each separate line print the largest number formed by arranging the elements of the array in the form of a string.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 102
+0 ≤ A[i] ≤ 103
+
+Example:
+Input:
+2
+5
+3 30 34 5 9
+4
+54 546 548 60
+
+Output:
+9534330
+6054854654 
+*/
 namespace Largest_number_formed_from_array
 {
     public static class AppHelper
