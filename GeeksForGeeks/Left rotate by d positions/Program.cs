238d5d1ef53b45c6bfa03a65170fd3c5d04c658e
Chinmay <Chinmay8116@gmail.com>
2020-07-23 18:57:48 +0530

    commit-added method3 for left rotation by d times

--- GeeksForGeeks/Left rotate by d positions/Program.cs
+++ GeeksForGeeks/Left rotate by d positions/Program.cs
@@ -8,6 +8,31 @@ namespace Left_rotate_by_d_positions
 {
     public static class AppHelper
     {
+        private static void Swap(Int32 first,Int32 second)
+        {
+            Int32 temp = first;
+            first = second;
+            second = temp;
+        }
+        private static void Reverse(Int32 [] arr,Int32 d,Int32 n)
+        {
+            Int32 low = 0;
+            Int32 high = n - 1;
+            while(low<high)
+            {
+                Swap(arr[low], arr[high]);
+                low++;
+                high--;
+            }
+        }
+        public static void RotateMethod3(Int32 [] arr,Int32 d)
+        {
+            Int32 n = arr.Length;
+            Reverse(arr, 0, d - 1); //reverse only first d elements
+            Reverse(arr, d, n - 1); //reverse from d-n-1
+            Reverse(arr, 0, n - 1);  //reverse whole array
+        }
+
         public static void RotateMethod2(Int32 [] arr,Int32 d)
         {
             Int32[] temp = new int[d];
