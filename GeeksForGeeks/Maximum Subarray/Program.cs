51680d16d75998c14b14ecd33933b42e8a72e5a0
Chinmay <Chinmay8116@gmail.com>
2020-08-16 18:08:37 +0530

    max subarray

---
+++ GeeksForGeeks/Maximum Subarray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Subarray
+{
+    public static class AppHelper
+    {
+        public static Int32 MaxSubarraySum(Int32[] arr, Int32 n)
+        {
+            Int32 result = 0;
+            for (Int32 i = 0; i < n; i++)
+            {
+                Int32 current = 0;
+                for (Int32 j = i; j < n; j++)
+                {
+                    current = current + arr[j];
+                    result = Math.Max(current, result);
+                }               
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
