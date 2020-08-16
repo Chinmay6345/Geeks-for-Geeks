be08b46dc7c305c73bdd771bdb3ea1258be081c0
Chinmay <Chinmay8116@gmail.com>
2020-08-16 17:04:57 +0530

    commit-optimized approach

--- GeeksForGeeks/Tapping Rain Water/Program.cs
+++ GeeksForGeeks/Tapping Rain Water/Program.cs
@@ -23,6 +23,30 @@ namespace Tapping_Rain_Water
             }
             return result;
         }
+        public static Int32 OTappingWater(Int32 [] arr,Int32 n)
+        {
+            if(arr.Length==0)
+            {
+                return 0;
+            }
+            Int32 [] lmax = new Int32[n];
+            Int32 [] rmax = new Int32[n];
+            Int32 result = 0;
+            lmax[0] = arr[0];
+            for(Int32 i=1;i<n;i++)
+            {
+                lmax[i] = Math.Max(lmax[i-1], arr[i]);             
+            }
+            for(Int32 i=n-2;i>=0;i--)
+            {
+                rmax[i] = Math.Max(rmax[i+1], arr[i]);  
+            }
+            for(Int32 i=1;i<n-1;i++)
+            {
+                result= result+ Math.Min(lmax[i], rmax[i])- arr[i];             
+            }
+            return result;
+        }
     }
     class Program
     {
