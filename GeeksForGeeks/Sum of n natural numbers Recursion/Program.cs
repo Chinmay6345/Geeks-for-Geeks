f10265d98c24f037583edbbfa30a7b1f3932eab4
Chinmay <Chinmay8116@gmail.com>
2020-11-24 10:52:50 +0530

    commit -sum of n natural numbers recursion

---
+++ GeeksForGeeks/Sum of n natural numbers Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_n_natural_numbers_Recursion
+{
+    public static class AppHelper
+    {
+        public static Int32 SumOfN(Int32 n)
+        {
+                return n == 0 ? 0 : SumOfN(n - 1) + n;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AppHelper.SumOfN(3));
+            Console.ReadLine();
+        }
+    }
+}
