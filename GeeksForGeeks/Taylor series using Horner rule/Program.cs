1e238cd8744b4cf4f0e29ca56f2cfdafc72705f9
Chinmay <Chinmay8116@gmail.com>
2020-11-25 17:30:18 +0530

    commit-taylors series iteration

---
+++ GeeksForGeeks/Taylor series using Horner rule/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taylor_series_using_Horner_rule
+{
+    public static class AppHelper
+    {
+        public static double TaylorSeries(Int32 x, Int32 n)
+        {
+             double s = 1;
+            for (; n > 0; n--)
+            {
+                s = 1 + (x/n)*s;
+            }
+            return s;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(AppHelper.TaylorSeries(1, 10));
+            Console.ReadLine();
+        }
+    }
+}
