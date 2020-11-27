4d9b7af37e1cb684afedacdabdf07f93112a48c3
Chinmay <Chinmay8116@gmail.com>
2020-11-27 19:53:21 +0530

    Commit-Tower of Hanoi

---
+++ GeeksForGeeks/Tower of Hanoi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tower_of_Hanoi
+{
+    public static class AppHelper
+    {
+        public static void TowerOfHanoi(Int32 n,Char A,Char B,Char C)
+        {
+            if(n>0)
+            {
+                TowerOfHanoi(n - 1, A, C, B);
+                Console.WriteLine("Move disk " + n + " from " + A + " to " +  C);
+                TowerOfHanoi(n - 1, B, A, C);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppHelper.TowerOfHanoi(3, '1', '2', '3');
+            Console.ReadLine();
+        }
+    }
+}
