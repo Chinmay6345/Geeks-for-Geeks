4a9514f18258c52033891043ff1a1fc366861266
Chinmay <Chinmay8116@gmail.com>
2020-12-01 16:30:45 +0530

    commit-fib

---
+++ GeeksForGeeks/Fibnonacci Series/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibnonacci_Series
+{
+    public static class AppHelper
+    {
+        public static Int32 FibIteration(Int32 n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            int term0 = 0, term1 = 1, sum = 0;
+            for(Int32 i=2;i<=n;i++)
+            {
+                sum = term0 + term1;
+                term0 = term1;
+                term1 = sum;
+            }
+            return sum;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(AppHelper.FibIteration(7));
+            Console.ReadLine();
+        }
+    }
+}
