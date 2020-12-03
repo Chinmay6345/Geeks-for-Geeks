6c8450019a5e0656373632302cc5b5c145873423
Chinmay <Chinmay8116@gmail.com>
2020-12-03 14:55:53 +0530

    commit-jagged array eg

--- GeeksForGeeks/Basic ND array opereations/Program.cs
+++ GeeksForGeeks/Basic ND array opereations/Program.cs
@@ -20,6 +20,40 @@ namespace Basic_ND_array_opereations
                     Console.WriteLine("arr["+ i + " "+ j +"]= " + arr[i, j]);
                 }
             }
+            Console.WriteLine();
+
+            foreach(Int32 i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            String[,] arr1 = new String[3, 2] { { "a", "b"}, {"c", "d" }, { "e", "f" } };
+            
+            foreach(String i in arr1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            for(Int32 i=0;i<arr1.GetLength(0);i++)
+            {
+                for(int j=0;j<arr1.GetLength(1);j++)
+                {
+                    Console.WriteLine("arr["+ i + " "+ j +"]= " + arr1[i, j]);
+                }
+            }
+            int[][] arr2 = {
+                            new int[4]{11,12,13,14},
+                            new int[5]{21,22,23,24,25},
+                            new int[3]{31,32,33}
+                           };
+            for (int i = 0; i < arr2.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr2[i].Length; j++)
+                {
+                    Console.WriteLine(arr2[i][j] + " " + "and i ,j = " + i + " " +j);
+                }
+            }
             Console.ReadLine();
         }
     }
