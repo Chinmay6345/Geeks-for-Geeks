f1999b6062dc66d8d22ae61c89d4daf826eec819
Chinmay <Chinmay8116@gmail.com>
2020-12-05 19:14:33 +0530

    commit sorted array

---
+++ GeeksForGeeks/Check if array is sorted/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_array_is_sorted
+{
+    public static class AppHelper
+    {
+        public static bool IsArraySorted(Int32[] arr)
+        {
+            for (Int32 i = arr.Length - 1; i > 0; i--)
+            {
+                if (arr[i] < arr[i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool IsArraySortedDesc(Int32[] arr)
+        {
+            for (Int32 i = arr.Length - 1; i > 0; i--)
+            {
+                Console.WriteLine("arr[i-1] and arr[i] =" + arr[i - 1] + " " + arr[i] + " is less ?" + (arr[i - 1] < arr[i] ? true : false));
+                if (arr[i - 1] < arr[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //Console.WriteLine(AppHelper.IsArraySorted(arr));
+            arr = arr.OrderByDescending(g => g).ToArray<Int32>();
+            Console.WriteLine(AppHelper.IsArraySortedDesc(arr));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GeeksForGeeks/Sort Negative numbers followed by positive/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Negative_numbers_followed_by_positive
+{
+    public static class AppHelper
+    {
+        public static void SortArray(Int32[] arr)
+        {
+            Int32 i = 0, j = arr.Length - 1;
+            while(i<j)
+            {
+                while (arr[i] < 0)
+                    i++;
+                while (arr[j] >= 0)
+                    j--;
+                if(i<j)
+                {
+                    Int32 temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            Console.WriteLine(String.Join(",", arr.Select(g => g)));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32[] arr = { -1, 2, 3, 4, -5, 6, 7, 8, 9 };
+            AppHelper.SortArray(arr);
+            Console.ReadLine();
+        }
+    }
+}
