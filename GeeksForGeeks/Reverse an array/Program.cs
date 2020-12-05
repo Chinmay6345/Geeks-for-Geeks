0381b2d498b001082b93b49d5ca2c1c69cd2775a
Chinmay <Chinmay8116@gmail.com>
2020-12-05 11:31:55 +0530

    commit-reverse an array

---
+++ GeeksForGeeks/Reverse an array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_an_array
+{
+    public static class AppHelper
+    {
+        public static void leftrotateByOne(Int32 [] arr)
+        {
+            for(Int32 i=0,j=arr.Length-1;i<j;i++,j--)
+            {
+                Int32 temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            Console.WriteLine(String.Join(",", arr.Select(g => g)));
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32[] arr = { 1, 2, 3, 4, 5 };
+            AppHelper.leftrotateByOne(arr);
+            Console.ReadLine();
+        }
+    }
+}
