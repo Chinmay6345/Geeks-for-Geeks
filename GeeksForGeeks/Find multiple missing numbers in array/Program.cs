230380b727eac04bae8baca905db73bbcb3ee124
Chinmay <Chinmay8116@gmail.com>
2020-12-06 18:16:27 +0530

    commit-multiple missing numbers

---
+++ GeeksForGeeks/Find multiple missing numbers in array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_multiple_missing_numbers_in_array
+{
+    public static class AppHelper
+    {
+        public static void MissingNumbers(int[] arr)
+        {
+            Int32 difference = arr[0];
+            Int32 n = arr.Length;
+            for(Int32 i=0;i<n;i++)
+            {
+                if(arr[i]-i !=difference)
+                {
+                    while(difference<arr[i]-i)
+                    {
+                        Console.WriteLine("Missing number is " + (difference + i));
+                        difference += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppHelper.MissingNumbers(new int[] { 6,7,8,9,11,12,15,16,17,19});
+            Console.ReadLine();
+        }
+    }
+}
--- GeeksForGeeks/Missing Number/Program.cs
+++ GeeksForGeeks/Missing Number/Program.cs
@@ -15,13 +15,25 @@ namespace Missing_Number
             Int32 s = n * (n + 1) / 2;
             return s - sum;
         }
+        public static Int32 MissingNumberUsingSet(Int32 [] nums)
+        {
+            HashSet<Int32> Set = new HashSet<Int32>();
+            foreach (Int32 num in nums)
+                Set.Add(num);
+            for(Int32 i=0;i<nums.Length+1;i++)
+            {
+                if (!Set.Contains(i))
+                    return i;
+            }
+            return -1;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            int k = AppHelper.MissingNumber(new int[] { 9,6,4,2,3,5,7,0,1 });
+            int k = AppHelper.MissingNumberUsingSet(new int[] { 9,6,4,2,3,5,7,0,1 });
             Console.WriteLine(k);
             Console.ReadLine();
         }
